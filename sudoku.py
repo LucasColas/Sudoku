9e80af1804ca04f19f0b111d1b3e78a98b097785
MrRex <lucas.colas13@gmail.com>
2020-04-25 19:24:23 +0200

    update

---
+++ sudoku.py
@@ -0,0 +1,24 @@
+import pygame
+pygame.init()
+
+Width, Height = 1280, 720
+Name = "Sudoku"
+Win = pygame.display.set_mode((Width, Height))
+pygame.display.set_caption(Name)
+
+bg = (255, 255, 255)
+
+run = True
+
+def redraw_window():
+
+    Win.fill(bg)
+    pygame.display.update()
+
+while run:
+    redraw_window()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            quit()
