f938ef300ce7c5e7feadb803e68c97eeb5c15955
MrRex <lucas.colas13@gmail.com>
2020-04-25 19:34:25 +0200

    update

--- sudoku.py
+++ sudoku.py
@@ -10,15 +10,21 @@ bg = (255, 255, 255)
 
 run = True
 
+def grid():
+    for i in range(3):
+        pass 
+
+
 def redraw_window():
 
     Win.fill(bg)
     pygame.display.update()
 
-while run:
-    redraw_window()
+def main():
+    while run:
+        redraw_window()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-            quit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                quit()
